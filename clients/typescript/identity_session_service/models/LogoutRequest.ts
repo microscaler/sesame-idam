761d4c0d283786590ce23bbc8c046ad5c9d23f17
Charles Sibbald <casibbald@gmail.com>
2026-05-10 03:22:00 +0300

    feat(chore): cleanup typescript client that covers areas that should not have been in the client

--- clients/typescript/identity_session_service/models/LogoutRequest.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type LogoutRequest = {
-    refresh_token?: string;
-    session_id?: string;
-};
-
--- clients/typescript/identity_user_mgmt_service/models/OAuthLogoutRequest.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type OAuthLogoutRequest = {
-    post_logout_redirect_uri?: string;
-    id_token_hint?: string;
-};
-
